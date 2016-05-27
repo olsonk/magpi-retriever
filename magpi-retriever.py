7d7c1e2c45078a75a0e93622cb2cfb4e6078bdf1
Kevin Olson <olsonk@gmail.com>
2016-05-27 15:36:58 -0700

    Updated magpi-retriever.py for custom use
    Improved to handle custom start_ and current_ issues, added comments

--- magpi-retriever.py
+++ magpi-retriever.py
@@ -1,10 +1,11 @@
 import urllib
 
-issue = 39
+start_issue = 01				# edit this number for whatever issue you want to start with
+current_issue = 46				# edit this number to the last issue you want to download
 
-for i in range(47):
+for i in range(current_issue-start_issue+1):	# calculates the number of issues it should download
 	current = issue + i
 	url = "https://www.raspberrypi.org/magpi-issues/MagPi%02d.pdf" % current
 	filename = "MagPi%02d.pdf" % current
 	
-	urllib.urlretrieve(url, filename)
+	urllib.urlretrieve(url, filename)	# files are downloaded to the same directory as this script
